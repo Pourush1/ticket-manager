2c6aa3c485f4dd38a6f6792fe9a371a1d10d238a
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2025-06-25 19:20:50 -0500

    Refactor layout and update metadata for Ticket Manager; add navigation links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { ticketsPath } from "@/paths";
 const Home = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Ticket Manager App</h1>
       <Link href={ticketsPath} className="text-sm underline">
         Go To Tickets Page
       </Link>
