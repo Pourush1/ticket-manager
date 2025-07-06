fd5103af1c8b52625856846970810eb5c77a4191
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2025-07-05 19:19:39 -0500

    Refactor Tickets component to fetch tickets asynchronously; making it a server component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,21 +1,9 @@
-'use client';
-
-import { useEffect, useState } from 'react';
 import { Heading } from '@/components/heading';
 import { TicketItem } from '@/features/ticket/components/ticket-item';
 import { getTickets } from '@/features/ticket/queries/get-tickets';
-import { Ticket } from '@/features/ticket/types';
 
-const Tickets = () => {
-  const [tickets, setTickets] = useState<Ticket[]>([]);
-
-  useEffect(() => {
-    const fetchTickets = async () => {
-      const tickets = await getTickets();
-      setTickets(tickets);
-    };
-    fetchTickets();
-  }, []);
+const Tickets = async () => {
+  const tickets = await getTickets();
 
   return (
     <div className="flex flex-col flex-1 gap-y-8">
