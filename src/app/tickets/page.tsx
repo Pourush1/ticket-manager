aae6e269a20f3dba6e5aaf050fea4fffbbd38f72
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2025-07-05 21:03:47 -0500

    Add react-error-boundary dependency and update Tickets page to use Spinner as fallback in Suspense for improved loading experience.

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,12 +1,13 @@
 import { Heading } from '@/components/heading';
 import { Suspense } from 'react';
 import { TicketList } from '@/features/ticket/components/ticket-list';
+import { Spinner } from '@/components/spinner';
 
 const Tickets = async () => {
   return (
     <div className="flex flex-col flex-1 gap-y-8">
       <Heading title="Tickets" description="All your tickets are listed here" />
-      <Suspense>
+      <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
     </div>
---
+++ src/components/spinner.tsx
@@ -0,0 +1,14 @@
+import { LucideLoaderCircle } from 'lucide-react';
+
+const Spinner = () => {
+  return (
+    <div
+      role="status"
+      className="flex flex-1 flex-col items-center justify-center self-center"
+    >
+      <LucideLoaderCircle className="w-16 h-16 animate-spin" />
+    </div>
+  );
+};
+
+export { Spinner };
