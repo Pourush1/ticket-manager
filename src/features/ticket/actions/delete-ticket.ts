69d386468824e05fcee7bbfe2b42d2685ac223ee
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2025-07-26 11:38:55 -0500

    Push test commit

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { prisma } from '@/lib/prisma';
 import { ticketsPath } from '@/paths';
@@ -9,5 +10,6 @@ export const deleteTicket = async (id: string) => {
   await prisma.ticket.delete({
     where: { id },
   });
+  revalidatePath(ticketsPath());
   redirect(ticketsPath());
 };
--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,10 +1,13 @@
+import { cache } from 'react';
 import { prisma } from '@/lib/prisma';
 
-export const getTicket = async (id: string) => {
+export const getTicket = cache(async (id: string) => {
+  console.log('get ticket should be logged in the terminal', id);
   const ticket = await prisma.ticket.findUnique({
     where: {
       id,
     },
   });
+  consoe.log('ticket', ticket);
   return ticket;
-};
+});
